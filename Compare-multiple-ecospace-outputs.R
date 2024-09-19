2b3261e45fd1eb10bbb4f7947c8b8c76c487eb17
holdenharris-NOAA <holden.harris@noaa.gov>
2024-09-19 10:47:08 -0400

    Worked on bringing in monthly Ecospace data. Ecospace scenarios are read through the list by and brought in by month. Plot code was made (might not be working).

--- Compare-multiple-ecospace-outputs.R
+++ Compare-multiple-ecospace-outputs.R
@@ -28,6 +28,8 @@ num_plot_pages = 4 ## Sets number of pages for PDF file
 today_date <- format(Sys.Date(), "%Y-%m-%d")
 out_file_notes = "test-STEdrivers"
 init_years_toscale = 6 ## In plotting, this sets the "1 line" to the average of this number of years
+plot_name_xY = paste0("BxY_scaled_", init_years_toscale, "y-", out_file_notes, ".PDF")
+plot_name_xM = paste0("BxM_scaled_", init_years_toscale, "y-", out_file_notes, ".PDF")
 
 ## Set up output folders -------------------------------------------------------
 dir_out <- "./Scenario_comps/Test_EnvDrivers/" ## Folder where outputs will be stored
@@ -38,10 +40,10 @@ dir_pdf_out  = paste0(dir_out)  ## Folder for plots
 dir_tab_out  = paste0(dir_out) ## Folder for tables with fit metrics
 if (!dir.exists(dir_pdf_out)) dir.create(dir_pdf_out, recursive = TRUE) ## Create the folder if it doesn't exist
 if (!dir.exists(dir_tab_out)) dir.create(dir_tab_out, recursive = TRUE) ## Create the folder if it doesn't exist
-
 folder_name <- paste0(dir_tab_out, "Scaled_", init_years_toscale, "y") ## Folder name based on `init_years_toscale`
-(plot_name_xY = paste0("BxY_scaled_", init_years_toscale, "y-", out_file_notes, ".PDF"))
-pdf_file_name= paste0(dir_pdf_out, plot_name_xY)
+pdf_file_name_xY = paste0(dir_pdf_out, plot_name_xY)
+pdf_file_name_xM = paste0(dir_pdf_out, plot_name_xM)
+
 
 
   ## -----------------------------------------------------------------------------
@@ -61,18 +63,54 @@ pdf_file_name= paste0(dir_pdf_out, plot_name_xY)
   simC_xY <- read.csv(paste0(dir_sim, "catch_annual.csv"), skip = num_skip_sim)
   simC_xY$year.group = NULL
   
+  ## Prepare months and date series objects
+  start_y <- min(years)
+  end_y   <- max(years)
+  date_series <- seq(as.Date(paste0(start_y, "-01-01")), as.Date(paste0(end_y,   "-12-01")), by = "1 month")
+  year_series <- seq(as.Date(paste0(start_y, "-01-01")), as.Date(paste0(end_y,   "-12-01")), by = "1 year")
+  ym_series <- format(date_series, "%Y-%m")
+  
+  ## Read in Ecosim monthly biomasses
+  simB_xM <- read.csv(paste0(dir_sim, "biomass_monthly.csv"), skip = num_skip_sim); simB_xM$timestep.group = NULL
+  simC_xM <- read.csv(paste0(dir_sim, "catch_monthly.csv"), skip = num_skip_sim); simC_xM$timestep.group = NULL
+  rownames(spaB_xM) = rownames(simB_xM) = ym_series
+  
+  ## -----------------------------------------------------------------------------
+  ##
   ## Read-in Ecospace annual biomass and catches ---------------------------------
-  ls_spaB_xY <- list()
-  ls_spaC_xY <- list()
+  
+  ## Initialize empty lists (4) for biomasses and catches by year and month
+  ls_spaB_xY <- list(); ls_spaB_xM <- list(); ls_spaC_xY <- list(); ls_spaC_xM <- list() 
+  
+  ## Loop through scenarios to populate lists
   for (i in 1:length(spa_scenarios)) {
-    dir_spa = paste0("./", ewe_name, "/", spa_scenarios[i], "/")
-    filename <- paste0(dir_spa, "Ecospace_Annual_Average_Biomass.csv")
-    num_skip_spa <- f.find_start_line(filename, flag = "Year")
-    spaB_xY <- read.csv(paste0(dir_spa, "Ecospace_Annual_Average_Biomass.csv"), 
-                        skip = num_skip_spa, header = TRUE)
-    spaB_xY$Year = NULL
+    (dir_spa = paste0("./", ewe_name, "/", spa_scenarios[i], "/"))
     
-    ## Standardize FG names ------------------------------
+    ## Read in Annual Biomass
+    filename <- paste0("Ecospace_Annual_Average_Biomass.csv")    ## Set filename
+    filepath <- paste0(dir_spa, filename)                        ## Set filepath based on directory 
+    num_skip_spa <- f.find_start_line(filepath, flag = "Year")   ## Apply function to find the start line to start reading table
+    spaB_xY <- read.csv(filepath, skip = num_skip_spa, header = TRUE); spaB_xY$Year = NULL
+    
+    ## Read in Monthly Biomass
+    filename <- paste0("Ecospace_Average_Biomass.csv")
+    filepath <- paste0(dir_spa, filename)
+    num_skip_spa <- f.find_start_line(filepath, flag = "TimeStep")
+    spaB_xM <- read.csv(filepath, skip = num_skip_spa, header = TRUE); spaB_xM$TimeStep = NULL
+    
+    ## Read in Annual Catches
+    filename <- paste0("Ecospace_Annual_Average_Catch.csv")        ## Set filename
+    filepath <- paste0(dir_spa, filename)                          ## Set filepath based on directory 
+    num_skip_spa <- f.find_start_line(filepath, flag = "Year") ## Apply function to find the start line to start reading table
+    spaC_xY <- read.csv(filepath, skip = num_skip_spa, header = TRUE); spaC_xY$Year = NULL
+    
+    ## Read in Monthly Catches
+    filename <- paste0("Ecospace_Average_Catch.csv")
+    filepath <- paste0(dir_spa, filename)
+    num_skip_spa <- f.find_start_line(filepath, flag = "TimeStep")
+    spaC_xM <- read.csv(filepath, skip = num_skip_spa, header = TRUE); spaC_xM$TimeStep = NULL
+    
+    ## Standardize Ecospace FG names ------------------------------
     fg_names = f.standardize_group_names(colnames(spaB_xY))
     num_fg = length(fg_names)
     fg_df <- data.frame(
@@ -82,39 +120,21 @@ pdf_file_name= paste0(dir_pdf_out, plot_name_xY)
     )
     
     ## Set row and column names
-    rownames(spaB_xY) = rownames(simB_xY) = years
-    colnames(spaB_xY) = colnames(simB_xY) = fg_df$group_name
+    rownames(spaB_xY) = rownames(spaC_xY) = years
+    rownames(spaB_xM) = rownames(spaC_xM) = ym_series
+    colnames(spaB_xM) = colnames(simB_xY) = fg_df$group_name
     
-    ## Add current spaB_xY reading into the list object ----
-    ls_spaB_xY[[i]] <- spaB_xY
-    
-    ## Read-in Ecospace annual catch
-    spaC_xY <- read.csv(paste0(dir_spa, "Ecospace_Annual_Average_Catch.csv"), 
-                        skip = num_skip_spa, header = TRUE)
-    spaC_xY$Year = NULL
-    ls_spaC_xY[[i]] <- spaC_xY
+    ## Add to list
+    ls_spaB_xY[[i]] <- spaB_xY; ls_spaB_xM[[i]] <- spaB_xM
+    ls_spaC_xY[[i]] <- spaC_xY; ls_spaC_xM[[i]] <- spaC_xM
   }
   
-  ## -----------------------------------------------------------------------------
-  ## Prepare months and date series objects
-  start_y <- min(years)
-  end_y   <- max(years)
-  date_series <- seq(as.Date(paste0(start_y, "-01-01")), as.Date(paste0(end_y,   "-12-01")), by = "1 month")
-  year_series <- seq(as.Date(paste0(start_y, "-01-01")), as.Date(paste0(end_y,   "-12-01")), by = "1 year")
-  ym_series <- format(date_series, "%Y-%m")
-  
-  ## Read in MONTHLY biomasses
-  #simB_xM <- read.csv(paste0(dir_sim, "biomass_monthly.csv"), skip = num_skip_sim); simB_xM$timestep.group = NULL
-  #simC_xM <- read.csv(paste0(dir_sim, "catch_monthly.csv"), skip = num_skip_sim); simC_xM$timestep.group = NULL
-  #spaB_xM <- read.csv(paste0(dir_spa, "Ecospace_Average_Biomass.csv"), skip = num_skip_spa, header = TRUE); spaB_xM$TimeStep = NULL
-  #rownames(spaB_xM) = rownames(simB_xM) = ym_series
-  
-  ## -----------------------------------------------------------------------------
+  ## ---------------------------------------------------------------------------
   ##
-  ## Read in OBSERVED timeseries -----------------------------------------------
+  ## Read in OBSERVED timeseries 
   dir_obs = paste0("./", ewe_name, "/", obs_TS_name, ".csv")
   obs.list = f.read_ecosim_timeseries(dir_obs, num_row_header = 4)
-  for(i in 1:length(obs.list)){assign(names(obs.list)[i],obs.list[[i]])} #make separate dataframe for each list element
+  for(i in 1:length(obs.list)){assign(names(obs.list)[i],obs.list[[i]])} ## Make separate dataframe for each list element
   
   obsB.head <- merge(obsB.head, fg_df, by = "pool_code", all.x = TRUE)
   obsC.head <- merge(obsC.head, fg_df, by = "pool_code", all.x = TRUE)
@@ -323,6 +343,8 @@ pdf_file_name= paste0(dir_pdf_out, plot_name_xY)
   ##
   ## Plot biomasses
   ## Note: Make sure PDF readers are closed before running pdf()
+  
+  ## Setup for plots -----------------------------------------------------------
 
   ## Plotting parameters
   col_obs = 'black'
@@ -330,15 +352,28 @@ pdf_file_name= paste0(dir_pdf_out, plot_name_xY)
   col_spa <- c("darkgoldenrod", "indianred2", "steelblue4", "darkorchid4", "darkgreen")
   #col_spa <- adjustcolor(col_spa, alpha.f = 1) ## Adjust transparancy
   
-  x = year_series
   num_plot_pages = 4; x_break = 5; y_break = 4; x_cex = 0.9; y_cex = 0.9; x_las = 2;
   sim_lty = 1; spa_lty = 1
   sim_lwd = 2; spa_lwd = 1; obs_pch = 16; obs_cex = 0.8;
   main_cex = 0.85; leg_cex = 0.9; leg_pos = 'topleft';leg_inset = 0.1
   #simB_scaled = spaB_scaled_ls
   
-  print(paste("Writing", pdf_file_name))
-  pdf(pdf_file_name, onefile = TRUE)
+  ## Set number of plots per page
+  set.mfrow = f.get_plot_dims(x=num_fg / num_plot_pages, round2=4)
+  par(mfrow=set.mfrow, mar=c(1, 2, 1, 2))
+  plots_per_pg = set.mfrow[1] * set.mfrow[2]
+  
+  
+  
+  
+  
+  ## -----------------------------------------------------------------------------
+  ##
+  ## Plot by Year (xY)
+  
+  print(paste("Writing", pdf_file_name_xY))
+  pdf(pdf_file_name_xY, onefile = TRUE)
+  x = year_series
   
   ## Set number of plots per page
   set.mfrow = f.get_plot_dims(x=num_fg / num_plot_pages, round2=4)
@@ -346,7 +381,7 @@ pdf_file_name= paste0(dir_pdf_out, plot_name_xY)
   plots_per_pg = set.mfrow[1] * set.mfrow[2]
   
   for(i in 1:num_fg){
-    #  for(i in 1:19){
+      #for(i in 1:19){
     grp  = fg_df$group_name[i]
     simB = simB_xY[,i] 
     spaB_ls <- lapply(ls_spaB_xY, function(df) df[, i]) ## Extract the i column from each data frame in the list
@@ -429,3 +464,111 @@ pdf_file_name= paste0(dir_pdf_out, plot_name_xY)
     }
   }
   dev.off()    
+  
+  
+
+  
+    
+  ## -----------------------------------------------------------------------------
+  ##
+  ## Plot by month (xM)
+
+  print(paste("Writing", pdf_file_name_xM))
+  pdf(pdf_file_name_xM, onefile = TRUE)
+  
+  ## Set number of plots per page
+  set.mfrow = f.get_plot_dims(x=num_fg / num_plot_pages, round2=4)
+  par(mfrow=set.mfrow, mar=c(1, 2, 1, 2))
+  plots_per_pg = set.mfrow[1] * set.mfrow[2]
+  
+  for(i in 1:num_fg){
+    #  for(i in 1:19){
+    grp  = fg_df$group_name[i]
+    simB = simB_xM[,i] 
+    spaB_ls <- lapply(ls_spaB_xM, function(df) df[, i]) ## Extract the i column from each data frame in the list
+    
+  
+    ## Check to see if observed data is available
+    obsB_scaled=NULL
+    if(i %in% obsB.head$pool_code){
+      obs.idx     = which(obsB.head$pool_code==i)
+      obs_df      = suppressWarnings( ## Suppress warnings thrown when obs not available
+        data.frame(year_series, obsB = as.numeric(obsB[ ,obs.idx]))
+      )
+      obsB_scaled = obs_df$obsB / mean(na.omit(obs_df$obsB)[1:init_years_toscale], na.rm = TRUE)
+      rm(obs_df)
+      rm(obs.idx)
+    }
+    
+    ## Scale to the average of a given timeframe
+    init_months_toscale = init_years_toscale * 12
+    spaB_scaled = spaB / mean(spaB[1:init_months_toscale], na.rm = TRUE)
+    spaB_scaled_ls = list()
+    for(j in 1:length(spa_scenarios)){
+      spaB               <- spaB_ls[[j]]
+      spaB_scaled        <- spaB / mean(spaB[1:init_years_toscale], na.rm = TRUE)
+      spaB_scaled_ls[[j]] <- spaB_scaled
+    }
+   
+    ##-------------------------------------------------------------------------------  
+    ## PLOT 
+    
+    ## Legend plots -------------------------------------------
+    if(i %in% seq(1, num_fg, by = plots_per_pg-1)) {
+      plot(0, 0, type='n', xlim=c(0,1), ylim=c(0,1), xaxt='n', yaxt='n', 
+           xlab='', ylab='', bty='n') # Create an empty plot
+      legend(leg_pos, inset = 0.1, bg="gray90", box.lty = 0,
+             legend=c('Observed','Ecosim', spa_scen_names),
+             lty = c(NA, sim_lty, rep(spa_lty, length(spaB_scaled_ls))), 
+             lwd = c(NA, sim_lwd+1, rep(spa_lwd+1, length(spaB_scaled_ls))),
+             pch=c(obs_pch, NA, rep(NA, length(spaB_scaled_ls))), 
+             col =c(col_obs, col_sim, col_spa), 
+             cex = leg_cex)
+    }
+    
+    ## Data plots -------------------------------------------
+    ## Determine y-axis range and initialize plot
+    min = min(obsB_scaled, simB_scaled, unlist(spaB_scaled_ls), na.rm=T) * 0.8
+    max = max(obsB_scaled, simB_scaled, unlist(spaB_scaled_ls), na.rm=T) * 1.2
+    plot(x, rep("", length(x)), type='b', 
+         ylim = c(min, max), xaxt = 'n', yaxt = 'n',
+         xlab = '', ylab='', bty = 'n')
+    title(main = grp, line=-.6, cex.main = main_cex) ## Add title
+    
+    ## Get years from date series
+    posx = as.POSIXlt(date_series)
+    x_years = unique(posx$year + 1900)
+    end_y = max(x_years)
+    start_y = min(x_years)
+    
+    ## Setup X-axis
+    year_series <- seq(as.Date(paste0(start_y, "-01-01")), as.Date(paste0(end_y,   "-12-01")), by = "1 year")
+    num_breaks_x <- round((end_y - start_y) / x_break) ## Determine x-axis breaks
+    x_ticks <- pretty(x, n = num_breaks_x)
+    xlab = paste0("'", substring(format(x_ticks, "%Y"), nchar(format(x_ticks, "%Y")) - 1))
+    axis(1, at = x_ticks, labels = xlab, cex.axis = x_cex, las = x_las)
+    
+    ## Setup Y-axis
+    y_ticks = pretty(seq(min, max, by = (max-min)/10), n = y_break)
+    axis(2, at = y_ticks, labels = y_ticks, las = 1, cex.axis = y_cex)
+    abline(h=1, col='lightgray')
+    
+    ## Plot outputs: Ecosim (green line), Ecospace (other lines), Observed (black dots)
+    if(length(obsB_scaled)>0) points(year_series, obsB_scaled, pch=16, cex=obs_cex, col = col_obs) ## Plot observed data, if it's present
+    lines(x = ym_series, simB_scaled, lty=sim_lty, lwd = sim_lwd,  col = col_sim) ## Plot Ecosim
+    if(is.list(spaB_scaled_ls)) {     ## If it's a list, loop through each element and plot
+      for(j in seq_along(spaB_scaled_ls)) {
+        lines(x = ym_series, spaB_scaled_ls[[j]], lty=spa_lty, lwd=spa_lwd, col=col_spa[j]) # Plot each Ecospace projection. Use the j-th color in the palette for each line.
+      }
+      #} else if(is.list(spaB_scaled_ls)==FALSE) { # If it's not a list, but a vector, plot directly
+      #  lines(x, spaB_scaled, lty=1, lwd=spa_lwd, col=col_spa[1]) # Plot Ecospace
+    }
+  }
+  dev.off()    
+  
+  
+  
+  
+  
+  
+  
